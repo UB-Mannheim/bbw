38a7b616e89ee12501d3e778790919296debd6f5
Jörg Mechnich <joerg.mechnich@bib.uni-mannheim.de>
2020-11-13 18:02:31 +0100

    bbw_cli.py: pretty-print error if any one of the files is not found

--- bbw_cli.py
+++ bbw_cli.py
@@ -23,39 +23,40 @@ path = ''
 nround = str(2)
 nsubmission = str(42)
 
-# Load the target data
 try:
+    # Load the target data
     target_cpa = pd.read_csv(path+f"target/CPA_Round{nround}_Targets.csv", names=['file', 'column0', 'column'], dtype=object)
     target_cta = pd.read_csv(path+f"target/CTA_Round{nround}_Targets.csv", names=['file', 'column'], dtype=object)
     target_cea = pd.read_csv(path+f"target/CEA_Round{nround}_Targets.csv", names=['file', 'row', 'column'], dtype=object)
+
+    # Create a list of filenames for matching in CPA, CEA and CTA tasks
+    filelist = target_cpa.file.to_list() + target_cea.file.to_list() + target_cta.file.to_list()
+    filelist = sorted(list(set(filelist)))
+    if args.amount is None:
+        args.amount = len(filelist)
+    if args.offset is None:
+        args.offset = 0
+    filelist = filelist[args.offset:args.offset + args.amount]
+
+    if __name__ == "__main__":
+        print(args)
+        cpa, cea, nomatch = [], [], []
+        # Annotate files from filelist
+        for ind, filename in enumerate(tqdm(filelist)):
+            filecsv = pd.read_csv(path+f'tables_round{nround}/'+filename+'.csv', dtype=str, header=None)
+            filecsv = preprocessing(filecsv)
+            [cpa, cea, nomatch] = contextual_matching(filename, filecsv, cpa, cea, nomatch, 
+                                                      step3=False, step4=False, step5=True, step6=True)
+        # Postprocess cpa and cea lists and return the ready-for-submission dataframes
+        [cpa_sub, cea_sub, cta_sub] = postprocessing(cpa, cea, filelist, 
+                                                     target_cpa, target_cea, target_cta)
+        # Save CP-, CE- and CT-Annotations to csv-files
+        now = time.time() # It separates the outputs of parallel runs in different folders
+        os.mkdir(f'r{nround}_s{nsubmission}_'+str(now))
+        cpa_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cpa_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
+        cea_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cea_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
+        cta_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cta_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
+
 except FileNotFoundError as e:
     print(e)
     sys.exit(1)
-
-# Create a list of filenames for matching in CPA, CEA and CTA tasks
-filelist = target_cpa.file.to_list() + target_cea.file.to_list() + target_cta.file.to_list()
-filelist = sorted(list(set(filelist)))
-if args.amount is None:
-    args.amount = len(filelist)
-if args.offset is None:
-    args.offset = 0
-filelist = filelist[args.offset:args.offset + args.amount]
-
-if __name__ == "__main__":
-    print(args)
-    cpa, cea, nomatch = [], [], []
-    # Annotate files from filelist
-    for ind, filename in enumerate(tqdm(filelist)):
-        filecsv = pd.read_csv(path+f'tables_round{nround}/'+filename+'.csv', dtype=str, header=None)
-        filecsv = preprocessing(filecsv)
-        [cpa, cea, nomatch] = contextual_matching(filename, filecsv, cpa, cea, nomatch, 
-                                                  step3=False, step4=False, step5=True, step6=True)
-    # Postprocess cpa and cea lists and return the ready-for-submission dataframes
-    [cpa_sub, cea_sub, cta_sub] = postprocessing(cpa, cea, filelist, 
-                                                 target_cpa, target_cea, target_cta)
-    # Save CP-, CE- and CT-Annotations to csv-files
-    now = time.time() # It separates the outputs of parallel runs in different folders
-    os.mkdir(f'r{nround}_s{nsubmission}_'+str(now))
-    cpa_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cpa_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
-    cea_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cea_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
-    cta_sub.to_csv(f'r{nround}_s{nsubmission}_{now}/bbw_r{nround}_s{nsubmission}_cta_sub.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
